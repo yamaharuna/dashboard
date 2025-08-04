91df81592cb4d3045d2a7e8cd0008815cc4e4d7e
yamaharuna <harunack17@icloud.com>
2025-08-04 17:43:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import LineChart from './conponents/lineChart';
-import PieChart from './conponents/pieChart';
+import PieChart1 from './conponents/pieChart1';
+import PieChart2 from './conponents/pieChart2';
+import PieChart3 from './conponents/pieChart3';
+import Pulldown from './conponents/Pulldown';
+
 
 function App() {
   return (
     <div className="App">
       <h2>Line Chart</h2>
       <LineChart />
-      <h2>Pie Chart</h2>
-      <PieChart />
-    </div>
+
+     
+
+<Pulldown />
+<div className="pie-row">
+<h2>Pie Charts</h2>
+  <PieChart1 />
+  <h2>Pie Charts</h2>
+  <PieChart2 />
+  </div>
+  <div className="pie-single">
+  <h2>Pie Charts</h2>
+  <PieChart3 />
+  </div>
+  
+</div>
   );
 }
 
